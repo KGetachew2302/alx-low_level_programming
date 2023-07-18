5d7652ba82f4ff31e509c8d63d94240caab8d1a8
Kaleab Getachew <kalegetachew24@gmail.com>
2023-07-18 09:03:37 -0700

    x

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
 /*
 *main function
 */
@@ -18,5 +15,3 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-
-#endif
